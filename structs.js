3032a9c3dd7bffcb4daaf68f777a1f05a4dba99e
micke-dahlgren <micke.dahlgren@gmail.com>
2021-03-09 07:49:57 +0100

    added structs

--- structs.js
+++ structs.js
@@ -7,4 +7,23 @@ class Color {
   }
 }
 
-export { Color }
+class Vec {
+  constructor(x,y,z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}
+
+class Triangle {
+  constructor(p0, p1, p2) {
+    if (p0.constructor.name !== 'Vec') {
+      console.log('err')
+    }; 
+    this.p0 = p0;
+    this.p1 = p1;
+    this.p2 = p2;
+  }
+}
+
+export { Color, Vec, Triangle }
