e410d21eaa38793f23e4230cca67920017f33b7a
micke-dahlgren <micke.dahlgren@gmail.com>
2021-03-09 07:39:11 +0100

    cleaned up main

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 import { RenderWindow, renderFrame} from './draw.js'
 
-let win = new RenderWindow(128, 128);
+const win = new RenderWindow(128, 128);
 let frameData = win.context.getImageData(0, 0, win.width, win.height);
 
-let update = function () {
-  renderFrame(frameData.data);
-  win.context.putImageData(frameData, 0, 0);
-  renderNextFrame();
-};
 
 function renderNextFrame() {
-  window.requestAnimationFrame(update);
+  renderFrame(frameData.data);
+  win.context.putImageData(frameData, 0, 0);
+  window.requestAnimationFrame(renderNextFrame);
 };
 
-renderNextFrame();
+function main() {
+  renderNextFrame();
+}
+
+main();
